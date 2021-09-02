89c3d41381c9d655d20a000f655cf9ea53b8c4d5
Steven Manwaring <89216732+stevenmanwaring@users.noreply.github.com>
2021-09-02 18:25:57 -0400

    iterate through bad words array instead of using array.split

--- subjectSearch.js
+++ subjectSearch.js
@@ -4,14 +4,13 @@ function checkSubject() {
 	let input = document.getElementById("textbox").value;
 	input = input.toLowerCase();
 
-	let subjectArray = input.split(' ');
+	//let subjectArray = input.split(' ');
 	
 	const resultArray = [];
 
-	for (let i=0; i < subjectArray.length; i++) {
-			if (badWords.includes(subjectArray[i])) {
-			console.log('found elem');
-			resultArray.push(subjectArray[i]);
+	for (let i=0; i < badWords.length; i++) {
+			if (input.includes(badWords[i])) {
+			resultArray.push(badWords[i]);
 		}
 	}
 	printResult(resultArray);
@@ -21,10 +20,3 @@ function printResult(resultArray) {
 	const result = resultArray.join(' ');
 	document.getElementById('result').innerHTML = result;
 }
-
-/* 
-
-iterate through bad words and check against input string? 
-
-*/
-
